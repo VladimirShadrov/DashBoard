b56e92e2f348672a07f34e0a4a5d2e1ba8c541a7
Vladimir <shadrov_vladimir@mail.ru>
2021-03-02 18:32:03 +0300

    Добавлена верстка левого блока

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import '../styles/styles.scss';
+import '../styles/left-bar.scss';
+
+// Перенос изображений
+require.context('../images', true, /\.(png|jpg|svg|gif)$/);
+require.context('../fonts', true, /\.(ttf|woff|woff2)$/);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const webpack = require("webpack");
-const autoprefixer = require("autoprefixer");
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -8,17 +8,17 @@ const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plug
 const TerserWebpackPlugin = require('terser-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
-const isProd = !isDev
+const isProd = !isDev;
 
 const optimization = () => {
   const config = {};
   if (isProd) {
     config.minimizer = [
       new OptimizeCssAssetsWebpackPlugin(),
-      new TerserWebpackPlugin()
-    ]
+      new TerserWebpackPlugin(),
+    ];
   }
-  return config
+  return config;
 };
 
 const excludeSourceMap = () => {
@@ -31,32 +31,29 @@ module.exports = {
   mode: 'development',
   context: path.resolve(__dirname, 'src'),
   entry: {
-    main: ['@babel/polyfill', './js/index.js']
+    main: ['@babel/polyfill', './js/index.js'],
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HTMLWebpackPlugin({
-      template: './index.html'
+      template: './index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: 'styles/styles.css'
+      filename: 'styles/styles.css',
     }),
     new webpack.LoaderOptionsPlugin({
       options: {
-        postcss: [
-          autoprefixer()
-        ]
-      }
+        postcss: [autoprefixer()],
+      },
     }),
     new webpack.SourceMapDevToolPlugin({
       filename: '[file].map',
-      exclude: excludeSourceMap()
+      exclude: excludeSourceMap(),
     }),
-
   ],
   module: {
     rules: [
@@ -70,11 +67,11 @@ module.exports = {
               sourceMap: true,
               url: false,
               import: true,
-            }
+            },
           },
           'postcss-loader',
-          'sass-loader'
-        ]
+          'sass-loader',
+        ],
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
@@ -83,9 +80,20 @@ module.exports = {
             loader: 'file-loader',
             options: {
               name: '[path][name].[ext]',
-            }
-          }
-        ]
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+        ],
       },
       {
         test: /\.m?js$/,
@@ -93,18 +101,16 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
   },
   optimization: optimization(),
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
     port: 3080,
-    open: true
+    open: true,
   },
-
-}
-
+};
